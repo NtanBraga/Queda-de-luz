30e01467bb293d873527836a2fe8e90332f7d22e
NtanBraga <natanybraga@gmail.com>
2026-03-11 17:55:59 -0300

    Propotipo chat

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,6 +3,15 @@ import { onMounted, ref } from 'vue'
 import { initMap } from './scripts/map.ts'
 
 const openMenu = ref(true)
+const openChat = ref(true)
+const newMessage = ref('')
+
+const sendMessage = () => {
+  if(newMessage.value.trim()) {
+    newMessage.value= '';
+  }
+}
+
 
 onMounted(async () => {
   await initMap('map-canvas')
@@ -10,7 +19,7 @@ onMounted(async () => {
 </script>
 
 <style lang="scss">
-@import './assets/styles/App.scss';
+@use './assets/styles/App.scss';
 </style>
 
 <template>
@@ -19,6 +28,7 @@ onMounted(async () => {
   </div>
   <div class="below-content">
     <button v-if="!openMenu" @click="openMenu = true" class="button-power-outage-outside">P</button>
+    <button v-if="!openChat" @click="openChat = true" class="button-chat-outside"></button>
     <div class="box-power-outage" :class="{ 'is-hidden': !openMenu }">
       <div class="box-power-outage-header">
         <h2 class="box-power-outrage-h2">Bairros sem luz</h2>
@@ -33,5 +43,25 @@ onMounted(async () => {
       </ul>
     </div>
     <div class="box-map" id="map-canvas"><h1>Map</h1></div>
+    <div class="box-chat" :class="{ 'isHidden': !openChat}">
+      <div class="box-chat-header">
+        <h2 class="box-chat-header-h2">
+          CHAT COMUNITARIO
+        </h2>
+        <button class="box-chat-button" @click="openChat = false">X</button>
+      </div>
+      <div class="box-chat-messages">
+        <div class="message"><strong>User:</strong> Ta sem luz</div>
+      </div>
+      <div class="box-chat-input">
+        <input
+          v-model="newMessage"
+          type="text"
+          placeholder="Digite algo..."
+          @keyup.enter="sendMessage"
+        />
+        <button @click="sendMessage">➤</button>
+      </div>
+    </div>
   </div>
 </template>
