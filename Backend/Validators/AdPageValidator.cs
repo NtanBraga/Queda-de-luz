608ba305f8643c77d8ae5ad2fa33e1b7ad33b322
Pedro Zancan <pedro.zancan18@gmail.com>
2026-04-20 18:39:03 -0300

    district existence validation added in the GET active ads endpoint

--- Backend/Validators/AdPageValidator.cs
+++ Backend/Validators/AdPageValidator.cs
@@ -1,3 +1,5 @@
+using Dapper;
+
 public  class AdPageValidator
 {
     private IBlackoutMapConnectionFactory _connectionFactory;
@@ -9,6 +11,7 @@ public  class AdPageValidator
     public async Task<(bool, RequestError?)> IsValid(GetActiveAdsRequest_QueryParams requestParams)
     {
         RequestError? error = null;
+        using var dbContext = await this._connectionFactory.CreateConnectionAsync();
 
         if(requestParams.district_Id is null)
         {
@@ -19,14 +22,25 @@ public  class AdPageValidator
             return (false, error);
         }
 
-        // if(requestParams.district_Id is null)
-        // {
-        //     error = new RequestError(
-        //         StatusCodes.Status404NotFound,
-        //         $"district [{requestParams.district_Id}] Not Found"
-        //     );
-        //     return (false, error);
-        // }
+        bool districtExists = await dbContext.QueryFirstAsync<bool>(
+            """
+                SELECT (EXISTS(
+                    SELECT * FROM District WHERE District_id = @districtId
+                )) AS District_Exists;
+            """,
+            new{districtId = requestParams.district_Id}
+        );
+
+        await dbContext.CloseAsync();
+
+        if(districtExists == false)
+        {
+            error = new RequestError(
+                StatusCodes.Status404NotFound,
+                $"district [{requestParams.district_Id}] Not Found"
+            );
+            return (false, error);
+        }
 
         return (true, null);
     } 
